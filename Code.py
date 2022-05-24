2baa7b3989efd334ee92c1d31831854063bef9ae
wylesnat <104783916+wylesnat@users.noreply.github.com>
2022-05-24 20:03:50 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,10 +3,27 @@ import pandas as pd
 import yfinance as yf
 import matplotlib as plt
 
-tickers = ["SPY", "^TNX"]
+def analysis2():
+    tickers= ["SPY","^TNX"]
+    data =yf.download(tickers, start="2000-01-01",end="2022-01-01", period='ld')
+    print (data.head(5))
 
-data = yf.download(tickers, start="2000-01-01", end = "2022-01-01", period="1d")
-print(data)
+
+#    fig = plt.figure()
+#    ax = fig.add_axes([0, 0, 1, 1])
+#    ax.scatter(data.iloc[:,0], data.iloc[:,1], color='r')
+#    ax.set_xlabel('Market')
+#    ax.set_ylabel('Interest rate')
+#    ax.set_title('Scatter plot')
+#    plt.show()
+
+    plt.scatter(data.iloc[:,0], data.iloc[:,1])
+    plt.title('Stock Market Return Vs Interest Rate')
+    plt.xlabel('Stock market return')
+    plt.ylabel('Interest rate')
+    plt.show()
+    plt.savefig("Output.png")
 
 
+analysis2()
 
